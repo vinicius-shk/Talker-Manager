2163e06db3da1704442a1347ba1fd6bd765e146f
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-10-04 15:34:09 -0300

    Conclusão do requisito 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const router = require('./routes');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,11 +8,14 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// não remova esse endpoint, e para o avaliador funcionar - PR
+// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+// app.use(express.json());
+app.use(router);
+
 app.listen(PORT, () => {
-  console.log('Online');
+  console.log('Online na porta 3000');
 });
