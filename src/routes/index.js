59495fd9612e5216115eba4ad78ddb3b5d701cd9
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-10-04 15:42:02 -0300

    Conclusão do req 2, arquivos faltando

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,21 @@ const path = require('path');
 
 const router = express.Router();
 
-router.get('/talker', async (req, res) => {
+router.get('/talker', async (_req, res) => {
   const response = await fs.readFile(path.resolve(__dirname, '../talker.json'));
   const data = await JSON.parse(response);
   res.status(200).json(data);
+});
+
+router.get('/talker/:id', async (req, res) => {
+  const id = Number(req.params.id);
+  const response = await fs.readFile(path.resolve(__dirname, '../talker.json'));
+  const data = await JSON.parse(response);
+  const getById = data.filter((talker) => talker.id === id);
+  if (getById.length === 0) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(200).json(getById[0]);
 });
 
 module.exports = router;
